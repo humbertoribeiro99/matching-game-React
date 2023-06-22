41a0f435323f47a56fd0f83c618d1bc6cebb5216
Humberto Eduardo Ribeiro da Silva <humberto.silva@corendf.gov.br>
2023-06-22 15:18:11 -0300

    Criado componente dos players

--- matching-game/main.js
+++ matching-game/main.js
@@ -1,3 +1,4 @@
+import PlayerName from "./src/components/PlayerName";
 import BoardGame from "./src/objetcs/BoardGame";
 import "./src/styles/elements/base.css"
 import "./src/styles/generic/reset.css"
@@ -6,9 +7,16 @@ import "./src/styles/settings/colors.css";
 
 
 //quando colocamos $ em uma variavel é para dizer que se refere ao Dom na tela
-const $root = document.querySelector("#root")
-const $htmlBoardGame = BoardGame(6);
+const $root = document.querySelector("#root");
 
 
 //$root.textContent = "olá mundo!" 
-$root.insertAdjacentHTML("beforeend",$htmlBoardGame)//metodo seguro para mostrar na tela do seu html
+$root.insertAdjacentHTML(
+    "beforeend", 
+    `
+    ${PlayerName('Player1')}
+    ${PlayerName('Player2')}
+    ${BoardGame(6)}
+    `
+
+)//metodo seguro para mostrar na tela do seu html
