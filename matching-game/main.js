fbd0d685809feb2baf37cfe050fce1771d77c7a4
Humberto Eduardo Ribeiro da Silva <humberto.silva@corendf.gov.br>
2023-06-22 10:23:30 -0300

    adicionado objetcs em src

--- matching-game/main.js
+++ matching-game/main.js
@@ -1,12 +1,14 @@
+import BoardGame from "./src/objetcs/BoardGame";
 import "./src/styles/elements/base.css"
 import "./src/styles/generic/reset.css"
 import "./src/styles/settings/colors.css";
-import CardGame from "./src/components/CardGame";
+
 
 
 //quando colocamos $ em uma variavel é para dizer que se refere ao Dom na tela
 const $root = document.querySelector("#root")
-const $htmlCardGame = CardGame()
+const $htmlBoardGame = BoardGame(6);
+
 
 //$root.textContent = "olá mundo!" 
-$root.insertAdjacentHTML("beforeend",$htmlCardGame)//metodo seguro para mostrar na tela do seu html
+$root.insertAdjacentHTML("beforeend",$htmlBoardGame)//metodo seguro para mostrar na tela do seu html
