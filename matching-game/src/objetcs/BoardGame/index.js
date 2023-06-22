4556ba6a7cdbbc09a1f16c4d8cc6f241cd0f2be1
Humberto Eduardo Ribeiro da Silva <humberto.silva@corendf.gov.br>
2023-06-22 14:59:06 -0300

    Mudança em base e estilo do body

--- matching-game/src/objetcs/BoardGame/index.js
+++ matching-game/src/objetcs/BoardGame/index.js
@@ -1,10 +1,15 @@
+import './style.css'
 import CardGame from "../../components/CardGame"
 
 function BoardGame(amountCards) {
     const $htmlCardGame = CardGame()
     const $htmlBoardGame = $htmlCardGame.repeat(amountCards)
    
-    return $htmlBoardGame;
+    return ` 
+    <section class="board-game">
+        ${$htmlBoardGame}
+    </section>
+    `
 }
 
 export default BoardGame
